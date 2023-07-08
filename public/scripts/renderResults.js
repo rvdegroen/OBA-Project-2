f787feeb8a62f80e6d5f1ea2899eb47fa877a8a8
rvdegroen <rvdegroen@gmail.com>
2023-07-08 16:08:08 +0200

    added function for when there are no results

--- public/scripts/renderResults.js
+++ public/scripts/renderResults.js
@@ -2,20 +2,33 @@ export function renderResults(results) {
     // show parent_results when searching for books and stuffs
     document.getElementById('parent_results').classList.remove('hidden');
     const resultsDiv = document.getElementById('results');
-
-    resultsDiv.innerHTML = ''; // Clear previous results
+    // Clear previous results
+    resultsDiv.innerHTML = '';
 
     const resultsGroupedByFormat = {};
 
+    console.log(results);
+
+    // 1 item of each searchResult
     for (let searchResult of results) {
+        // find the format from formats.text
         const [firstFormat] = searchResult.formats;
         const format = firstFormat.text;
 
+        // group the results witht the same format
         if (resultsGroupedByFormat[format]) {
             resultsGroupedByFormat[format].push(searchResult);
         } else {
             resultsGroupedByFormat[format] = [searchResult];
         }
+    }
+
+    // check if there are no results
+    if (Object.keys(resultsGroupedByFormat).length === 0) {
+        const noResultsMessage = document.createElement('h1');
+        noResultsMessage.textContent = 'Geen resultaten gevonden.';
+        resultsDiv.appendChild(noResultsMessage);
+        return;
     }
 
     // for every different format (book, cd)
@@ -35,6 +48,7 @@ export function renderResults(results) {
         for (let searchResult of resultsByFormat) {
             // add the items underneath the heading
             // creating js elements
+            // console.log(searchResult);
             const image = document.createElement('img');
             const container = document.createElement('a');
             const div = document.createElement('div');
@@ -48,11 +62,24 @@ export function renderResults(results) {
             const authors = shortenAuthor(searchResult.authors);
 
             // rendering
-            image.src = searchResult.coverimages[1];
             container.setAttribute('href', `/details/${searchResult.id}`); // go to /details/${object.id}
 
-            // appends
+            // if there's an coverimages available, then show and put the source, otherwise don't
+            // this is only for courses, since the output is different than for books
+            if (searchResult.coverimages[0]) {
+                image.src = searchResult.coverimages[0];
+            }
 
+            // if there's an coverimages available and there's more than 0, then show and put the source, otherwise don't
+            // this is for books and cd's for example
+            if (
+                searchResult.coverimages[1] &&
+                searchResult.coverimages.length > 0
+            ) {
+                image.src = searchResult.coverimages[1];
+            }
+
+            // appends
             container.appendChild(div);
             div.appendChild(imageContainer);
             imageContainer.appendChild(image);
