fb7df5e21724024300878bf6c7458e73e442abb1
rvdegroen <rvdegroen@gmail.com>
2023-07-10 23:59:38 +0200

    fixed id detail link for other formats too

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -11,16 +11,19 @@ exports.favorite = function (req, res) {
 exports.details = async function (req, res) {
     const id = req.params.id;
 
+    console.log(id);
+
     // /details/3453456
     // make sure there's an id
     if (!id) {
         return res.status(400).send('No id provided');
     }
 
-    // gets data from obaService in function getDetails
-    const record = await obaService.getDetails(id);
+    try {
+        // gets data from obaService in function getDetails
+        const record = await obaService.getDetails(id);
+        console.log('id', id);
 
-    console.log('Rendering record', record);
-
-    res.render('pages/details', { record });
+        res.render('pages/details', { record });
+    } catch (error) {}
 };
--- routes/frontend.js
+++ routes/frontend.js
@@ -5,7 +5,7 @@ const frontendController = require('../controllers/frontendController');
 // different pages get a route
 router.route('/').get(frontendController.index);
 // /details/someid
-router.route('/details/:id').get(frontendController.details);
+router.route('/details/:id(*)').get(frontendController.details);
 
 // favorite pages to /favorieten
 router.route('/favorieten').get(frontendController.favorite);
