a984d2b1d3e8007cab48cd4e7831d5abd3d435bd
rvdegroen <rvdegroen@gmail.com>
2023-07-12 20:02:44 +0200

    added cursor animation

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -4,8 +4,10 @@ const input = document.getElementById('query');
 if (input) {
     input.addEventListener('focus', () => {
         const jumpingDiv = document.querySelector('.jumping');
+        const lottieCursor = document.querySelector('.lottie-input-cursor');
         if (jumpingDiv) {
             jumpingDiv.classList.remove('jumping');
+            lottieCursor.classList.add('hidden');
         }
     });
 }
