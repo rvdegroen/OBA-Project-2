9838b131776df91f7cdad941cbe2c66142b97fa7
rvdegroen <rvdegroen@gmail.com>
2023-07-13 00:20:16 +0200

    added more animations

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -5,9 +5,16 @@ if (input) {
     input.addEventListener('focus', () => {
         const jumpingDiv = document.querySelector('.jumping');
         const lottieCursor = document.querySelector('.lottie-input-cursor');
-        if (jumpingDiv) {
+        const lottieHand = document.querySelector('.lottie-input-hand');
+
+        if (jumpingDiv && lottieCursor) {
             jumpingDiv.classList.remove('jumping');
             lottieCursor.classList.add('hidden');
+        }
+
+        if (jumpingDiv && lottieHand) {
+            jumpingDiv.classList.remove('jumping');
+            lottieHand.classList.add('hidden');
         }
     });
 }
